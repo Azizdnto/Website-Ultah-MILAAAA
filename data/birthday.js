26b09e22c737fe24a072fd7c89b2a70d36fb938c
azizdnto <azizdnto@gmail.com>
2026-06-19 13:43:01 +0700

    update versi baru

--- data/birthday.js
+++ data/birthday.js
@@ -2,7 +2,7 @@
 // Format: new Date(tahun, bulan, tanggal, jam, menit, detik)
 // CATATAN: bulan dimulai dari 0 -> 0 = Januari, 11 = Desember.
 // Contoh di bawah ini = 2 Desember 2026, jam 00:00.
-export const TARGET = new Date(2026, 11, 2, 0, 0, 0);
+export const TARGET = new Date(2026, 0, 2, 0, 0, 0);
 
 // true kalau waktu sekarang sudah melewati TARGET (kejutan boleh dibuka).
 export function isUnlocked() {
